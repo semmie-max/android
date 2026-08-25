96097cef18cd855956bcf650dc838c589deaafff
semmie-max <semmie162l@gmail.com>
2026-08-25 20:19:13 +0100

    ui

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/android",
+  assetPrefix: "/android/",
   images: {
     unoptimized: true,
   },
