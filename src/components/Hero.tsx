049de2c340fdd7843201d2c75fd8b45d74dc75d4
semmie-max <semmie162l@gmail.com>
2026-08-27 12:56:07 +0100

    add capabilities section to landing page

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -314,6 +314,7 @@ export default function LandingPage() {
     <main className="bg-black min-h-screen">
       <Hero />
       <ClaritySection />
+      <CapabilitiesSection />
     </main>
   );
 }
